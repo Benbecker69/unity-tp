1d2fe84c6ef2b36591fffce769c3307b529a3170
Ilias <ilias.developeur@gmail.com>
2025-07-25 10:36:06 +0200

    TP finit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,84 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
-public class NewEmptyCSharpScript
+public class GameManager : MonoBehaviour
 {
-    
+    public float tempsDeJeu = 45f;
+    public TextMeshProUGUI timerText;
+    public GameObject panelGameOver;
+    public GameObject panelVictoire;
+
+    private float tempsRestant;
+    private int totalCollectibles;
+    private int collectiblesRamasses = 0;
+    private bool jeuTermine = false;
+
+    void Start()
+    {
+        Time.timeScale = 1f; 
+        tempsRestant = tempsDeJeu;
+        totalCollectibles = GameObject.FindGameObjectsWithTag("Collectible").Length;
+
+        
+        if (panelGameOver) panelGameOver.SetActive(false);
+        if (panelVictoire) panelVictoire.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (!jeuTermine)
+        {
+            
+            tempsRestant -= Time.deltaTime;
+
+            
+            if (timerText)
+                timerText.text = "Temps: " + Mathf.Ceil(tempsRestant).ToString();
+
+            
+            if (tempsRestant <= 0)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    public void CollecterObjet()
+    {
+        collectiblesRamasses++;
+
+        
+        if (collectiblesRamasses >= totalCollectibles)
+        {
+            Victoire();
+        }
+    }
+
+    public void GameOver()
+    {
+        jeuTermine = true;
+        if (panelGameOver) panelGameOver.SetActive(true);
+        Time.timeScale = 0f; 
+    }
+
+    public void Victoire()
+    {
+        jeuTermine = true;
+        if (panelVictoire) panelVictoire.SetActive(true);
+        Time.timeScale = 0f; 
+    }
+
+    public void Recommencer()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("Jeu");
+    }
+
+    public void RetourMenu()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("Menu");
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ public class PlayerController : MonoBehaviour
 {
     public float vitesse = 5f;
     private Rigidbody rb;
+    private GameManager gameManager;
+    private Vector3 scaleOriginale;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        scaleOriginale = transform.localScale;
     }
 
     void Update()
     {
-       
+        
+        if (transform.position.y < -5f)
+        {
+            if (gameManager != null)
+                gameManager.GameOver();
+            return;
+        }
+
+        
         float horizontal = 0f;
         float vertical = 0f;
 
@@ -22,14 +34,61 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             horizontal = 1f;
 
-        
+
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             vertical = 1f;
         else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             vertical = -1f;
 
-       
+     
         Vector3 mouvement = new Vector3(horizontal, 0f, vertical);
         rb.linearVelocity = new Vector3(mouvement.x * vitesse, rb.linearVelocity.y, mouvement.z * vitesse);
+
+        
+        if (mouvement.magnitude > 0.1f) 
+        {
+            
+            float angleInclinaison = 15f;
+            transform.rotation = Quaternion.Euler(vertical * angleInclinaison, 0, -horizontal * angleInclinaison);
+
+            
+            transform.localScale = scaleOriginale;
+        }
+        else 
+        {
+
+            float respiration = Mathf.Sin(Time.time * 3f) * 0.05f + 1f;
+            transform.localScale = new Vector3(
+                scaleOriginale.x * respiration,
+                scaleOriginale.y,
+                scaleOriginale.z * respiration
+            );
+
+          
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, Time.deltaTime * 5f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+            
+            Destroy(other.gameObject);
+
+            
+            if (gameManager != null)
+                gameManager.CollecterObjet();
+        }
+    }
+
+    void OnDisable()
+    {
+
+        if (transform != null && scaleOriginale != Vector3.zero)
+        {
+            transform.localScale = scaleOriginale;
+            transform.rotation = Quaternion.identity;
+        }
     }
 }
